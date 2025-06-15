64a3be104a53f8f4fecc4c686d52279d8b5b5896
Mati G <matikgawin@gmail.com>
2025-06-15 17:04:01 +0200

    added input preparations

---
+++ DataCreation.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+import numpy as np 
+from tmm import coh_tmm
+
+
+#INPUT PREPARATIONS
+path = ''
+K = False      #True/false depending on k column inside the csv
+material = ''  #name of the material 
+gragas = []    #thickness 
+kat = 70       #angel degree
+index = 1      #refractive index of the material
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
