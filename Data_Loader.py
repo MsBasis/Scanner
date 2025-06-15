d456a31274380e70887d0f0e6a92c60180b9c5a2
Mati G <matikgawin@gmail.com>
2025-06-16 00:28:46 +0200

    Ultraviolence <3

--- Data_Loader.py
+++ Data_Loader.py
@@ -14,7 +14,12 @@ def dara_loaders(csv, test_size=0.2, batch_size=128):
     
     encoder = OneHotEncoder(sparse_output=False)
     material_encoded = encoder.fit_transform(x_raw[['material']])
+    x_num = x_raw.drop(columns=['material']).values
+    x_bigmom = np.concatenate([x_num, material_encoded], axis=1)
+    scaler = StandardScaler()
+    x_scaled = scaler.fit_transform(x_bigmom)
     
+    x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=test_size, random_state=42)
 
 
 
