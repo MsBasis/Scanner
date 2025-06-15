a106ce5522b35d49b1af404d372a411af28fccec
Mati G <matikgawin@gmail.com>
2025-06-15 18:30:17 +0200

    added neseser configurations

---
+++ DataConfiguration.py
@@ -0,0 +1,108 @@
+#needed for data preparationm
+
+configList = [
+    {
+        "material": "SiO2",
+        "csv_path": "",
+        "K": False,
+        "gragas": [50, 100, 150, 200, 250, 300, 400, 500],
+        "kat": 70,
+        "index": 1.4585
+    },
+    {
+        "material": "Al2O3",
+        "csv_path": "",
+        "K": False,
+        "gragas": [50, 100, 150, 200, 250, 300, 400, 500],
+        "kat": 70,
+        "index": 1.7673
+    },
+    {
+        "material": "TiO2",
+        "csv_path": "",
+        "K": True,
+        "gragas": [30, 60, 90, 120, 150, 180, 210, 240],
+        "kat": 70,
+        "index": 2.4881
+    },
+    {
+        "material": "ZnO",
+        "csv_path": "",
+        "K": True,
+        "gragas": [40, 80, 120, 160, 200, 240, 280, 320],
+        "kat": 70,
+        "index": 2.0127
+    },
+    {
+        "material": "Si",
+        "csv_path": "",
+        "K": True,
+        "gragas": [20, 40, 60, 80, 100, 150, 200, 250],
+        "kat": 70,
+        "index": 3.8814
+    },
+    {
+        "material": "GaAs",
+        "csv_path": "",
+        "K": True,
+        "gragas": [30, 60, 90, 120, 150, 180, 210, 240],
+        "kat": 70,
+        "index": 3.8207
+    },
+    {
+        "material": "Ge",
+        "csv_path": "",
+        "K": True,
+        "gragas": [30, 60, 90, 120, 150, 180, 210, 240],
+        "kat": 70,
+        "index": 4.241
+    },
+    {
+        "material": "ITO",
+        "csv_path": "",
+        "K": True,
+        "gragas": [50, 75, 100, 125, 150, 200, 250, 300],
+        "kat": 70,
+        "index": 1.8270
+    },
+    {
+        "material": "Au",
+        "csv_path": "",
+        "K": True,
+        "gragas": [10, 20, 30, 40, 50, 60, 70, 80],
+        "kat": 70,
+        "index": 0.47
+    },
+    {
+        "material": "Ag",
+        "csv_path": "",
+        "K": True,
+        "gragas": [10, 20, 30, 40, 50, 60, 70, 80],
+        "kat": 70,
+        "index": 0.1359
+    },
+    {
+        "material": "Cr",
+        "csv_path": "",
+        "K": True,
+        "gragas": [10, 20, 30, 40, 50, 60, 70, 80],
+        "kat": 70,
+        "index": 3.03
+    },
+    {
+        "material": "Ni",
+        "csv_path": "",
+        "K": True,
+        "gragas": [10, 20, 30, 40, 50, 60, 70, 80],
+        "kat": 70,
+        "index": 1.9648
+    }
+]
+
+
+
+
+
+
+
+
