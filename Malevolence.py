310d5e51ba0085a97ae30be75ba6bc928c1bddef
Mati G <matikgawin@gmail.com>
2025-06-16 00:51:36 +0200

    Ultraviolence <3

--- Malevolence.py
+++ Malevolence.py
@@ -1,7 +1,21 @@
 #Better late than never
+import torch 
+import torch.nn as nn 
 
 
-
+class MLP(nn.Module):
+    def __init__(slef, input_dim):
+        super(MLP, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128,64),
+            nn.ReLU(),
+            nn.Linear(64,2)
+        )
+        
+    def forward(self,x):
+        return self.net(x)
 
 
 
