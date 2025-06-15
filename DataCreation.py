bd1ca284f4122ebb7024bcbb14571a4bce031c4f
Mati G <matikgawin@gmail.com>
2025-06-15 21:24:04 +0200

    simulating variables

--- DataCreation.py
+++ DataCreation.py
@@ -1,15 +1,26 @@
 import pandas as pd 
 import numpy as np 
 from tmm import coh_tmm
+from DataConfiguration import configList
 
 
-#INPUT PREPARATIONS
-path = ''
-K = False      #True/false depending on k column inside the csv
-material = ''  #name of the material 
-gragas = []    #thickness 
-kat = 70       #angel degree
-index = 1      #refractive index of the material
+all = []
+
+def psi_delta(superior_n,wavelength,angle,n,d):
+    n_list = [1.0,superior_n,n]
+    d_list = [np.inf,d,np.inf]
+    optic = coh_tmm('p',n_list,d_list,angle,wavelength)
+    
+    odbicie = optic['r']
+    psi_tan = abs(odbicie)
+    delta_rad = np.angle(odbicie)
+    psi_deg = np.degrees(np.arctan(psi_tan))
+    delta_deg = np.degrees(delta_rad) % 360
+    return psi_deg, delta_deg
+
+
+
+
 
 
 
