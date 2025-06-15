cb1192abd116408c270ad63c4209415e956ab692
Mati G <matikgawin@gmail.com>
2025-06-15 23:47:23 +0200

    biblioteks

---
+++ Data_Loader.py
@@ -0,0 +1,23 @@
+import numpy as np 
+import pandas as pd 
+import torch
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
